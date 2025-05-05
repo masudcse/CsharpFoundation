1e2b923a2e8054ca053701d0bd429614006d2854
masudcse <mia.mahmudunnabi@petronas.com>
2025-05-06 00:13:24 +0800

    bubble short

---
+++ Sorting/sorting/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    //Sorting accedinign order 
+    public void AccesingOrder(int[] input)
+    {
+        //5
+        bool swapped;
+        for (int i = 0; i < input.Length - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < input.Length - i - 1; j++)
+            {
+                if (input[j] > input[j + 1])
+                {
+                    int temp = input[j];
+                    input[j] = input[j + 1];
+                    input[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+
+    }
+}
+
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = { 5, 1, 4, 2, 8 };
+        Solution solution = new Solution();
+        solution.AccesingOrder(input);
+        Console.WriteLine("Sorted"+string.Join(",",input));
+    }
+}
